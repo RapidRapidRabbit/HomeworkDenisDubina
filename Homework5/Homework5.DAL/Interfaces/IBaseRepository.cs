81f67a4444456e0a6ca2d2f6abd30b250daad668
MGKarisoll <job.myasnikov.gennady@gmail.com>
2021-09-18 12:03:16 +0300

    add feedback

--- Homework5/Homework5.DAL/Interfaces/IBaseRepository.cs
+++ Homework5/Homework5.DAL/Interfaces/IBaseRepository.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Не все CRUD операции объявлены:
+// 1. TEntity GetById(int id) - есть
+// 2. IEnumerable<TEntity> GetAll() - нет. Этот метод необходимо переопределять в конкретных репозиториях, чтобы включать необходимые свойства, если есть необходимость.
+// 3. TEntity Add(TEntity entity) - есть. Лучше возвращать результат создания сущности. В нашем случае это будет та же сущность, что и была передана в метод.
+// 4. TEntity Update(int id, TEntity entity) - нету. Базовый абстрактный класс должен иметь переопределяемый или абстрактный метод
+// 5. Delete(int id) - есть
 namespace Homework5.DAL.Interfaces
 {
    public  interface IBaseRepository<TEntity>
